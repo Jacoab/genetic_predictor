7cde71e5ea63bfd593bbdbbaf791d8cc5411b03b
Jacoab <jjongewaard1@gmail.com>
2017-08-16 19:36:09 -0600

    Edited get_expected_prob_dist function in the data_loader.py file.

--- src/data_proc.py
+++ src/data_proc.py
@@ -16,6 +16,25 @@ import numpy as np
 from src import data_loader as loader
 
 
+def _prob_of_next_word(next_word, prev_word_array, review_batch):
+    """
+    Calculates the probability of of a given word occuring after the previously
+    generated words.
+
+    :param next_word: Word to calculate the probability of
+    :return: The probability of next_word occuring after the previously generated words
+    """
+    compare_phrase = np.append(prev_word_array, next_word)
+    resized_batch = np.resize(review_batch, (len(compare_phrase)))
+    count = 0
+
+    for phrase in resized_batch:
+        if np.array_equal(phrase, compare_phrase):
+            count += 1
+
+    return count / (resized_batch.shape[0] * resized_batch.shape[1])
+
+
 def get_population_prob_dist(population, prev_word_array, review_batch):
     """
     Calculates the distribution of the probabilities of each word in the population
@@ -26,28 +45,10 @@ def get_population_prob_dist(population, prev_word_array, review_batch):
     :param review_batch: The batch of reviews that is being used to calculate the dist.
     :return: The probability distribution of the population
     """
-    def prob_of_next_word(next_word):
-        """
-        Calculates the probability of of a given word occuring after the previously
-        generated words.
-
-        :param next_word: Word to calculate the probability of
-        :return: The probability of next_word occuring after the previously generated words
-        """
-        compare_phrase = np.append(prev_word_array, next_word)
-        resized_batch = np.resize(review_batch, (len(compare_phrase)))
-        count = 0
-
-        for phrase in resized_batch:
-            if np.array_equal(phrase, compare_phrase):
-                count += 1
-
-        return count / (resized_batch.shape[0] * resized_batch.shape[1])
-
     prob_dist = {}
 
     for individual in population:
-        prob_dist[individual] = prob_of_next_word(individual)
+        prob_dist[individual] = _prob_of_next_word(individual, prev_word_array, review_batch)
 
     return prob_dist
 
@@ -65,14 +66,13 @@ def get_expected_prob_dist(prev_word_array, review_batch):
     all_words = loader.get_all_words(review_batch)
     all_words_ = list(map(lambda x: len(x), all_words))
     sorted_words = all_words[np.argsort(all_words)]
-    count = 0
 
-    for i in range(len(sorted_words)):
-        if sorted_words[i] == sorted_words[i+1]:
-            count += 1
+    for i in range(0, len(sorted_words) - 1):
+        if sorted_words[i + 1] == sorted_words[i]:
+            np.delete(sorted_words, i)
         else:
-            prob_dist[sorted_words[i-1]] = count
-            count = 0
+            prob_dist[sorted_words[i]] = _prob_of_next_word(sorted_words[i],
+                                                            prev_word_array, review_batch)
 
     return prob_dist
 
